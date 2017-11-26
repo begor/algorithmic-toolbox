7c0493f3dd39ba9877bd5291d56a07d9dd40a4e0
Biryukov Egor <birykovegor@gmail.com>
2017-11-26 18:21:06 +0300

    GCD

--- 02/gcd/gcd.cpp
+++ 02/gcd/gcd.cpp
@@ -1,46 +1,52 @@
 #include <iostream>
+#include <cstdlib>
 
-using namespace std;
-
-long simpleGCD(long a, long b) {
-    long min;
-    
-    if (a < b) {
-        min = a;
-    } else {
-        min = b;
+int gcd_naive(int a, int b) {
+  int current_gcd = 1;
+  for (int d = 2; d <= a && d <= b; d++) {
+    if (a % d == 0 && b % d == 0) {
+      if (d > current_gcd) {
+        current_gcd = d;
+      }
     }
-
-    while (min) {
-        if ((a % min == 0) && (b % min == 0)) {
-            break;
-        }
-        
-        --min;
-    }
-    
-    return min;
+  }
+  return current_gcd;
 }
 
-
-long euclidGCD(long a, long b) {
-    if (b == 0) { 
+int gcd(int a, int b) {
+    if (b == 0) {
         return a;
     }
 
-    long rem = a % b;
+    int mod = a % b;
 
-    return euclidGCD(b, rem);
+    return gcd(b, mod);
 }
 
+void test() {
+    while(true) {
+        int a = rand() % 1000 + 2;
+        int b = rand() % 1000 + 2;
 
+        std::cout << a << std::endl;
+        std::cout << b << std::endl;
+
+        int naive = gcd_naive(a, b);
+        int fast = gcd(a, b);
+
+        std::cout << naive << std::endl;
+        std::cout << fast << std::endl;
+
+        if (naive != fast) {
+            std::cout << "ERR" << std::endl;
+            break;
+        }
+    }
+}
 
 int main() {
-    long a, b;
-
-    cin >> a >> b;
-
-    cout << euclidGCD(a, b);
-
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << gcd(a, b) << std::endl;
     return 0;
 }
